2df0bb3fb3dddbfb64cb7ee8540c2c3eeede212a
Jonathan Alexis Montenegro <63118548+JonAleMonte1997@users.noreply.github.com>
2021-11-23 11:07:15 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MovieDescriptionComponent } from './component/movie-description/movie-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    MovieDescriptionComponent
   ],
   imports: [
     BrowserModule
